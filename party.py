ed467e50e93aef2d9e7be41850f52364c0efa135
Hackbright Student <no-reply@hackbrightacademy.com>
2017-11-02 15:05:19 -0700

    lab work and further study

---
+++ party.py
@@ -0,0 +1,169 @@
+"""Flask site for Balloonicorn's Party."""
+
+from flask import Flask, session, render_template, request, flash, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__)
+app.secret_key = "SECRETSECRETSECRET"
+
+
+def is_mel(email):
+    """Is this user Mel?
+
+    >>> is_mel('mel@ubermelon.com')
+    True
+    >>> is_mel('judith@awesome.com')
+    False
+    >>> is_mel('Mel@ubermelon.com')
+    True
+
+    """
+
+    return email.lower() == "mel@ubermelon.com"
+
+
+def most_and_least_common_type(treats):
+    """Given list of treats, return most and least common treat types.
+
+    Return most and least common treat types in tuple of format (most, least).
+        >>> treats = [
+        ...     {'type': 'dessert'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'drink'},
+        ... ]
+        >>> most_and_least_common_type(treats)
+        ('dessert', 'drink')
+
+    If no treats in list
+        >>> treats2 = []
+        >>> most_and_least_common_type(treats2)
+        (None, None)
+
+    If no least, returns both of most
+        >>> treats3 = [{'type': 'dessert'}, {'type': 'dessert'}]
+        >>> most_and_least_common_type(treats3)
+        ('dessert', 'dessert')
+
+    If only one item, returns as both most and least
+        >>> treats4 = [{'type': 'dessert'}]
+        >>> most_and_least_common_type(treats4)
+        ('dessert', 'dessert')
+
+    For tie, selects one of the tying items
+    Note: adjusted to handle the ordering
+        >>> treats5 = [
+        ...     {'type': 'dessert'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'drink'},
+        ...     {'type': 'appetizer'}
+        ... ]
+
+        >>> most_and_least_common_type(treats5) in [('dessert', 'drink'), ('dessert', 'appetizer')]
+        True
+
+
+    """
+
+    types = {}
+
+    # Count number of each type
+    for treat in treats:
+        types[treat['type']] = types.get(treat['type'], 0) + 1
+
+    most_count = most_type = None
+    least_count = least_type = None
+
+    # Find most, least common
+    for treat_type, count in sorted(types.items()):
+        if most_count is None or count > most_count:
+            most_count = count
+            most_type = treat_type
+
+        if least_count is None or count < least_count:
+            least_count = count
+            least_type = treat_type
+
+    return (most_type, least_type)
+
+
+def get_treats():
+    """Return treats being brought to the party.
+
+    One day, I'll move this into a database! -- Balloonicorn
+    """
+
+    return [
+        {'type': 'dessert',
+         'description': 'Chocolate mousse',
+         'who': 'Leslie'},
+        {'type': 'dessert',
+         'description': 'Cardamom-Pear pie',
+         'who': 'Joel'},
+        {'type': 'appetizer',
+         'description': 'Humboldt Fog cheese',
+         'who': 'Meggie'},
+        {'type': 'dessert',
+         'description': 'Lemon bars',
+         'who': 'Bonnie'},
+        {'type': 'appetizer',
+         'description': 'Mini-enchiladas',
+         'who': 'Katie'},
+        {'type': 'drink',
+         'description': 'Sangria',
+         'who': 'Anges'},
+        {'type': 'dessert',
+         'description': 'Chocolate-raisin cookies',
+         'who': 'Henry'},
+        {'type': 'dessert',
+         'description': 'Brownies',
+         'who': 'Sarah'}
+    ]
+
+
+@app.route("/")
+def homepage():
+    """Show homepage."""
+
+    return render_template("homepage.html")
+
+
+@app.route("/treats")
+def show_treats():
+    """Show treats people are bringing."""
+
+    treats = get_treats()
+
+    most, least = most_and_least_common_type(get_treats())
+
+    return render_template("treats.html",
+                           treats=treats,
+                           most=most,
+                           least=least)
+
+
+@app.route("/rsvp", methods=['POST'])
+def rsvp():
+    """Register for the party."""
+
+    name = request.form.get("name")
+    email = request.form.get("email")
+
+    if not is_mel(email):
+        session['rsvp'] = True
+        flash("Yay!")
+        return redirect("/")
+
+    else:
+        flash("Sorry, Mel. This is kind of awkward.")
+        return redirect("/")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    DebugToolbarExtension(app)
+    app.run()
